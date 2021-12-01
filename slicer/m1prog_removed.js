779a6d656af0e46c70e9def72bb8e4971d9a44c3
moemode <pfeffer.moritz@gmail.com>
2021-12-01 18:05:38 +0100

    Implement filtering an ast based on lines to keep (in slicer package)

---
+++ slicer/m1prog_removed.js
@@ -0,0 +1,6 @@
+function sliceMe() {
+    var x = 5;
+    if (x < 10)
+        x = x + 5;
+}
+sliceMe();
--- slicer/parser.js
+++ slicer/parser.js
@@ -2,6 +2,7 @@
 var acorn = require('acorn');
 var esc = require('escodegen')
 var fs = require('fs');
+var estrav = require('estraverse');
 
 function toAst(filePathIn, filePathOut) {
     let prog = fs.readFileSync(filePathIn).toString();
@@ -16,10 +17,24 @@ function toAst(filePathIn, filePathOut) {
  * @param {array of positive int} locations 
  */
 function keepLocations(program, locations) {
-    var ast = acorn.parse(prog, {ecmaVersion: 5, locations:true});
-    
-    let newprog = esc.generate(ast);
-    return 
+    var ast = acorn.parse(program, {ecmaVersion: 5, locations:true});
+    filtered_ast = estrav.replace(ast, {
+        leave: function(node, parent) {
+            if(!locations.includes(node.loc.start.line)) {
+                return this.remove();
+            }
+        }
+    });
+    let newprog = esc.generate(filtered_ast);
+    return newprog;
 }
 
-toAst("./slicer/m1prog.js", "./slicer/m1prog_trans.js")
+function removeLines(progInPath, progOutPath, linesToKeep) {
+    let prog = fs.readFileSync(progInPath).toString();
+    let newprog = keepLocations(prog, linesToKeep);
+    fs.writeFileSync(progOutPath, newprog);
+}
+
+//toAst("./slicer/m1prog.js", "./slicer/m1prog_trans.js")
+let linesToKeep = [1, 2, 5, 6, 9, 10]
+removeLines("./slicer/m1prog.js", "./slicer/m1prog_removed.js", linesToKeep)
