e77ec16cee4a46410e0b3215d87c2ce94014ca69
moemode <pfeffer.moritz@gmail.com>
2021-12-18 19:30:24 +0100

    Add m3b1 to testsuite

--- testcases/milestone3/b1_predicted_out.js
+++ testcases/milestone3/b1_predicted_out.js
@@ -1,10 +1,13 @@
 function sliceMe(number) {
     if (number === 1) {}  
     else if (number > 1) {
-        for (var i = 2; i < number; i++) {}
+        for (var i = 2; i < number; i++) {
+            if (number % i == 0) {
+                break;
+            }
+        }
     }
     else {}
-    console.log(status);
 }
 
 sliceMe(12);
