fffdb41c4efdc0593fa313a9b2d4576220bf406c
moemode <pfeffer.moritz@gmail.com>
2022-01-25 14:05:57 +0100

    Adds b1 trace and removes recast esprima mode

--- analysis/parser.js
+++ analysis/parser.js
@@ -1,34 +1,15 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prune = void 0;
 var fs_1 = require("fs");
 var datatypes_1 = require("./datatypes");
 var recast_1 = require("recast");
-var esprima = __importStar(require("esprima"));
 var ast_types_1 = require("ast-types");
 function pruneProgram(prog, lineNb, graph, relevantLocs, relevant_vars) {
-    var ast = (0, recast_1.parse)(prog, {
+    var ast = (0, recast_1.parse)(prog);
+    /* {
         parser: esprima,
-    });
+    })*/
     (0, ast_types_1.visit)(ast, {
         visitNode: function (path) {
             var node = path.node;
