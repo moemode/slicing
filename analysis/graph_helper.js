22ae034e1584f2d1759bcf31c073ae5a5ba2f09a
moemode <pfeffer.moritz@gmail.com>
2022-02-09 21:45:59 +0100

    Start work on graph helper

---
+++ analysis/graph_helper.js
@@ -0,0 +1,74 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GraphHelper = void 0;
+var datatypes_1 = require("./datatypes");
+var GraphHelper = /** @class */ (function () {
+    function GraphHelper(graph) {
+        this.nextNodeId = 1;
+        this.nextEdgeId = 1;
+        this.graph = graph;
+    }
+    GraphHelper.prototype.addWriteNode = function (rhsLoc, name, val, line) {
+        return this.addNode({
+            loc: rhsLoc,
+            name: name,
+            varname: name,
+            val: val,
+            type: "write",
+            line: line
+        });
+    };
+    GraphHelper.getNodesAt = function (nodes, loc) {
+        return nodes.filter(function (node) {
+            return datatypes_1.SourceLocation.in_between_inclusive(loc, node.data.loc);
+        });
+    };
+    GraphHelper.prototype.addNode = function (data, currentCallerLoc, testNode) {
+        var node = {
+            group: "nodes",
+            data: data
+        };
+        node.data.id = "n".concat(this.nextNodeId++);
+        if (currentCallerLoc) {
+            node.data.callerLoc = currentCallerLoc;
+        }
+        this.graph.add(node);
+        return node;
+    };
+    GraphHelper.prototype.addEdge = function (source, target) {
+        this.graph.add({
+            group: "edges",
+            data: {
+                id: "e".concat(this.nextEdgeId++),
+                source: source.data.id,
+                target: target.data.id
+            }
+        });
+    };
+    GraphHelper.prototype.addTestNode = function (test, result) {
+        var nodeData = {
+            data: {
+                loc: test.loc,
+                val: result,
+                line: test.loc.start.line,
+                type: "".concat(test.type, "-test"),
+                name: "".concat(test.type, "-test")
+            }
+        };
+        return this.addNode(nodeData);
+    };
+    GraphHelper.prototype.addBreakNode = function (loc) {
+        var breakNode = {
+            data: {
+                loc: loc,
+                line: loc.start.line,
+                name: "break"
+            }
+        };
+        var bNode = this.graph.add(breakNode);
+        return [breakNode, bNode];
+    };
+    return GraphHelper;
+}());
+exports.GraphHelper = GraphHelper;
+//# sourceMappingURL=graph_helper.js.map
---
+++ analysis/graph_helper.ts
@@ -0,0 +1,81 @@
+import cytoscape, { Core, ElementDefinition } from "cytoscape";
+import { SourceLocation } from "./datatypes";
+
+
+class GraphHelper {
+    graph: Core;
+    nextNodeId = 1;
+    nextEdgeId = 1;
+
+    constructor(graph: Core) {
+        this.graph = graph;
+    }
+
+    addWriteNode(rhsLoc, name, val, line): ElementDefinition {
+        return this.addNode({
+            loc: rhsLoc,
+            name,
+            varname: name,
+            val,
+            type: "write",
+            line
+        });
+    }
+
+    static getNodesAt(nodes: any[], loc: SourceLocation): any[] {
+        return nodes.filter((node) =>
+            SourceLocation.in_between_inclusive(loc, node.data.loc));
+    }
+
+
+     addNode(data, currentCallerLoc?, testNode?): ElementDefinition {
+        const node = {
+            group: <const> "nodes",
+            data: data
+        };
+        node.data.id = `n${this.nextNodeId++}`;
+        if (currentCallerLoc) {
+            node.data.callerLoc = currentCallerLoc;
+        }
+        this.graph.add(node);
+        return node;
+    }
+
+     addEdge(source, target): void {
+        this.graph.add({
+            group: <const> "edges",
+            data: {
+                id: `e${this.nextEdgeId++}`,
+                source: source.data.id,
+                target: target.data.id
+            }
+        });
+    }
+
+     addTestNode(test, result) {
+        const nodeData = {
+            data: {
+                loc: test.loc,
+                val: result,
+                line: test.loc.start.line,
+                type: `${test.type}-test`,
+                name: `${test.type}-test`
+            }
+        };
+        return this.addNode(nodeData);
+    }
+
+     addBreakNode(loc: SourceLocation) {
+        const breakNode = {
+            data: {
+                loc: loc,
+                line: loc.start.line,
+                name: `break`
+            }
+        };
+        const bNode: cytoscape.Collection = this.graph.add(breakNode);
+        return [breakNode, bNode];
+    }
+}
+
+export { GraphHelper };
